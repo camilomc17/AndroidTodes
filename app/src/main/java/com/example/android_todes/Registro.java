7abe47b96f60b788fd9f8fe086f061ccb18ce3e6
scastro368 <scastro368@misena.edu.co>
2022-10-22 07:17:15 -0500

    registro logueo

---
+++ app/src/main/java/com/example/android_todes/Registro.java
@@ -0,0 +1,17 @@
+package com.example.android_todes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Registro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro);
+    }
+
+
+
+}
