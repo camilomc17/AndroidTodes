6801389b35b6749ff78eb30bc8cdf28c400a6a20
unknown <ymmaya6@misena.edu.co>
2022-11-19 13:05:21 -0500

    Diseño Reporte

--- app/src/main/java/com/example/android_todes/FormularioReporte.java
+++ app/src/main/java/com/example/android_todes/FormularioReporte.java
@@ -2,13 +2,29 @@ package com.example.android_todes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class FormularioReporte extends AppCompatActivity {
 
+    FloatingActionButton btnCamara;
+
+    FloatingActionButton btnGaleria;
+    ImageView imgView;
+    private int requestCode;
+    private int resultCode;
+    private Intent data;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario_reporte);
+
+
     }
 }
