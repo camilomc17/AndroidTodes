e94c6f4905ee13005cb5317d1ec9fbca342692f6
jefferson <jefferson.quenan@misena.edu.co>
2022-11-24 20:55:54 -0500

    Se elimina DrawerMenu

--- app/src/main/java/com/example/android_todes/MainActivity.java
+++ app/src/main/java/com/example/android_todes/MainActivity.java
@@ -50,7 +50,7 @@ public class MainActivity extends AppCompatActivity {
         new Handler(getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent paginadashboard=new Intent(MainActivity.this,NavigationDrawerMenu.class);
+                Intent paginadashboard=new Intent(MainActivity.this,MainActivityNoticia.class);
                 startActivity(paginadashboard);
                 finish();
             }
