e83fd9570f81153eaff36cd28e9287d5b1362449
yuliepalechor <sanyulie08@gmail.com>
2022-11-30 19:18:21 -0500

    Manual primera subida

--- app/src/main/java/com/example/android_todes/eventos.java
+++ app/src/main/java/com/example/android_todes/eventos.java
@@ -7,18 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-    ImageButton devolver;
+public class eventos extends AppCompatActivity {
+    ImageButton anterior;
     ImageButton siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        siguiente=findViewById(R.id.imageButton2);
+        setContentView(R.layout.activity_eventos);
+        siguiente=findViewById(R.id.imageBtnE1);
+        anterior=findViewById(R.id.imageBtnE2);
     }
     public void Siguiente(View view){
-        Intent pasar=new Intent(this,eventos.class);
+        Intent pasar=new Intent(this,misreportes.class);
         startActivity(pasar);
+    }
+    public void Anterior(View view){
+        Intent antes=new Intent(this,MainActivity.class);
+        startActivity(antes);
     }
 }
--- app/src/main/java/com/example/android_todes/misreportes.java
+++ app/src/main/java/com/example/android_todes/misreportes.java
@@ -7,18 +7,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
-    ImageButton devolver;
+public class misreportes extends AppCompatActivity {
+    ImageButton anterior;
     ImageButton siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        siguiente=findViewById(R.id.imageButton2);
+        setContentView(R.layout.activity_misreportes);
+        siguiente=findViewById(R.id.imageButtonMR2);
+        anterior=findViewById(R.id.imageButtonMR1);
     }
     public void Siguiente(View view){
-        Intent pasar=new Intent(this,eventos.class);
+        Intent pasar=new Intent(this,crearreporte.class);
         startActivity(pasar);
+    }
+    public void Anterior(View view){
+        Intent antes= new Intent(this,eventos.class);
+        startActivity(antes);
     }
 }
