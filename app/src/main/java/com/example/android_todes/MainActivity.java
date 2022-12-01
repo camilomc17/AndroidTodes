2afe36da9bab9cdfd814bcf686719fee4cc9001f
scastro368 <scastro368@misena.edu.co>
2022-11-30 20:37:16 -0500

    quiro lo del main para hacer el merge

--- app/src/main/java/com/example/android_todes/MainActivity.java
+++ app/src/main/java/com/example/android_todes/MainActivity.java
@@ -17,26 +17,16 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
-
+ Button ir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                Intent i = new Intent(MainActivity.this,InicioSesion.class);
-                startActivity(i);
-            }
+         ir = findViewById(R.id.button_pasar);
 
-
-        },3000);
+        }
 
     }
 
 
-/*    public void PantallaLogueo(View view) {
-        Intent i = new Intent(this, InicioSesion.class);
-        startActivity(i);
-    }*/
-}
+
